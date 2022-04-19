c58edcc45c3364160fca36e8c9c7fe42adfea470
Israel Imru <imru@fixsrtars.com>
2022-04-19 15:25:54 -0700

    fixed year in milis

--- time_series_example.py
+++ time_series_example.py
@@ -6,7 +6,7 @@ import calendar
 import datetime
 
 factory = griddb.StoreFactory.get_instance()
-year_in_mili = 31556952000
+year_in_mili = 31536000000
 
 #Get GridStore object
 store = factory.get_store(
@@ -24,25 +24,23 @@ rs = query.fetch()
 
 data = rs.next()
 timestamp = calendar.timegm(data[0].timetuple())
-gsTS = (griddb.TimestampUtils.get_time_millis(timestamp));
+gsTS = (griddb.TimestampUtils.get_time_millis(timestamp))
+time = datetime.datetime.fromtimestamp(gsTS/1000.0)
+
 added = gsTS + (year_in_mili * 7)
+addedTime = datetime.datetime.fromtimestamp(added/1000.0)
 
-print(gsTS, added, type(gsTS), type(added))
-
-temp = datetime.datetime.fromtimestamp(gsTS/1000.0)
-yes = datetime.datetime.fromtimestamp(added/1000.0)
-
-x = ts.aggregate_time_series(temp, yes, griddb.Aggregation.AVERAGE, "value")
+x = ts.aggregate_time_series(time, addedTime, griddb.Aggregation.AVERAGE, "value")
 print("aggregation: ", x.get(griddb.Type.DOUBLE))
     
-rangeQuery = ts.query_by_time_series_range(temp, yes, griddb.QueryOrder.ASCENDING)
+rangeQuery = ts.query_by_time_series_range(time, addedTime, griddb.QueryOrder.ASCENDING)
 rangeRs = rangeQuery.fetch()
 while rangeRs.has_next():
     d = rangeRs.next()
     print("d: ", d)
   
 try:   
-    samplingQuery = ts.query_by_time_series_sampling(temp, yes, ["value"], griddb.InterpolationMode.LINEAR_OR_PREVIOUS, 1, griddb.TimeUnit.DAY)
+    samplingQuery = ts.query_by_time_series_sampling(time, addedTime, ["value"], griddb.InterpolationMode.LINEAR_OR_PREVIOUS, 1, griddb.TimeUnit.DAY)
     samplingRs = samplingQuery.fetch()
     while samplingRs.has_next(): 
         d = samplingRs.next()
