65edd62c7111f574611183d361665d475194905a
Israel Imru <imru@fixstars.com>
2022-05-04 23:51:08 +0000

    added in react files. fixed dockerfile to accept griddb related content in the app.js file

---
+++ blogs/blog_2/app.js
@@ -0,0 +1,33 @@
+const { config } = require('dotenv');
+const express = require('express');
+const { join } = require('path');
+const griddb = require('griddb_node');
+
+const app = express();
+
+var fs = require('fs');
+var factory = griddb.StoreFactory.getInstance();
+var store = factory.getStore({
+    "host": process.argv[2],
+    "port": parseInt(process.argv[3]),
+    "clusterName": process.argv[4],
+    "username": process.argv[5],
+    "password": process.argv[6]
+});
+
+config();
+
+
+app.use(express.static(join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
