822e3bce75a343ed4db391154b3ce03dd1da1422
imru@fixstars.com <imru@fixstars.com>
2022-07-14 10:11:55 -0700

    Adding in the sensor builder

--- blogs/4_react_docker_crud/frontend/src/App.js
+++ blogs/4_react_docker_crud/frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      const App = () =>  {    
 
+        useEffect(() => {
+            var xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = function () {
+
+                if (xhr.readyState !== 4) return;
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    let resp = JSON.parse(xhr.responseText);
+                    setData(resp.results)
+                }
+            };
+
+            xhr.open('GET', '/firstLoad');
+            xhr.send();
+
+        }, [])
+    
+
+        const handleSubmit = async (event) => {
+            fetch('/query', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({'testing': 'testing'})
+            }).then(function(response) {
+                console.log(response)
+                return response.json();
+            });
+            event.preventDefault()
+        }
+
+        return ( 
+               <div>
+                <form method="post" id="form" onSubmit={handleSubmit}>
+                 <label for="Cereals">Sensor Data</label>
+    
+                    <input type="submit" value="Submit Query" />
+                </form>
+                
+            </div>
+    )
+      }
+                
 export default App;
