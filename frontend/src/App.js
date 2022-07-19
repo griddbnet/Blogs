8e4165e33e00529e25de8a1ab2e779d9254e3385
imru@fixstars.com <imru@fixstars.com>
2022-07-18 19:36:35 -0700

    Fixed merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-
-      const App = () =>  {    
-
-        useEffect(() => {
-            var xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function () {
-
-                if (xhr.readyState !== 4) return;
-                if (xhr.status >= 200 && xhr.status < 300) {
-                    let resp = JSON.parse(xhr.responseText);
-                    setData(resp.results)
-                }
-            };
-
-            xhr.open('GET', '/firstLoad');
-            xhr.send();
-
-        }, [])
-    
-
-        const handleSubmit = async (event) => {
-            fetch('/query', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({'testing': 'testing'})
-            }).then(function(response) {
-                console.log(response)
-                return response.json();
-            });
-            event.preventDefault()
-        }
-
-        return ( 
-               <div>
-                <form method="post" id="form" onSubmit={handleSubmit}>
-                 <label for="Cereals">Sensor Data</label>
-    
-                    <input type="submit" value="Submit Query" />
-                </form>
-                
-            </div>
-    )
-      }
-                
-export default App;
-=======
 import React, { useState, useEffect, useCallback } from 'react';
 import Box from '@mui/material/Box';
 import {
@@ -262,4 +211,3 @@ const App = () => {
 }
 
 export default App;
->>>>>>> 1c40c684f238def8a9413f0cca53217896fc00b7
