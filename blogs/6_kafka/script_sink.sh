306d2bcc16756870aa8b10665f14d27a5a990738
Israel <imru@fixstars.com>
2022-09-27 11:07:51 -0700

    fixing up script and readme

--- blogs/6_kafka/script_sink.sh
+++ blogs/6_kafka/script_sink.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 function echo_payload {
-    echo '{"payload":{"ts":"'$1$2'","co":"'$3'","humidity":"'$4'","light":"'$5'","lpg":"'$6'","motion":"'$7'","smoke":"'$7'","temp":"'$7'"},"schema":{"fields":[{"field":"ts","optional":false,"type":"string"},{"field":"co","optional":false,"type":"FLOAT64"},{"field":"humidity","optional":false,"type":"FLOAT64"},{"field":"light","optional":false,"type":"BOOLEAN"},{"field":"lpg","optional":false,"type":"FLOAT64"},{"field":"motion","optional":false,"type":"BOOLEAN"}{"field":"smoke","optional":false,"type":"FLOAT64"},{"field":"temp","optional":false,"type":"FLOAT64"}],"name":"iot","optional":false,"type":"struct"}}'
-
+    echo '{"payload": {"ts": "'$1$2'","sensor":"'$3'","co": "'$4'","humidity": "'$5'","light": "'$6'","lpg": "'$7'","motion": "'$8'","smoke": "'$9'","temp": "'$10'"},"schema": {"fields": [{"field": "ts","optional": false,"type": "string"},{"field": "sensor","optional": false,"type": "string"},{"field": "co","optional": false,"type": "float64"},{"field": "humidity","optional": false,"type": "string"},{"field": "light","optional": false,"type": "string"},{"field": "lpg","optional": false,"type": "string"},{"field": "motion","optional": false,"type": "string"},{"field": "smoke","optional": false,"type": "string"},{"field": "temp","optional": false,"type": "string"}],"name": "iot","optional": false,"type": "struct"}}'
 }
 
 TOPICS=()
@@ -12,11 +11,11 @@ for file in `find $1 -name \*simulate_sensor.txt` ; do
     LOCATION="topic"
     head -10 $file |while read -r line ; do
         SENSOR=`echo ${line} | awk '{ print $3 }'`
-        if [[ ! " ${TOPICS[@]} " =~ " ${LOCATION}_${SENSOR} " ]]; then
-            echo Creating topic ${LOCATION}_${SENSOR}
-            kafka-topics.sh --bootstrap-server 127.0.0.1:9092 --create --topic  ${LOCATION}_${SENSOR} 2&>1 /dev/null
-            TOPICS+=(${LOCATION}_${SENSOR})
+        if [[ ! " ${TOPICS[@]} " =~ " ${SENSOR} " ]]; then
+            echo Creating topic ${SENSOR}
+            kafka-topics.sh --bootstrap-server 127.0.0.1:9092 --create --topic  ${SENSOR} 2&>1 /dev/null
+            TOPICS+=(${SENSOR})
         fi
-        echo_payload ${line} | kafka-console-producer.sh --topic ${LOCATION}_${SENSOR} --bootstrap-server localhost:9092
+        echo_payload ${line} | kafka-console-producer.sh --topic ${SENSOR} --bootstrap-server localhost:9092
     done
 done
