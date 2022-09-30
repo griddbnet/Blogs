56e535bc0d8c51f62bf67a7673161f4ce072eb1d
israel <imru@fixstars.com>
2022-09-30 15:55:59 -0700

    fixed up script

--- blogs/6_kafka/script_sink.sh
+++ blogs/6_kafka/script_sink.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 function echo_payload {
-    echo '{"payload": {"ts": "'$1$2'","sensor":"'$3'","co": "'$4'","humidity": "'$5'","light": "'$6'","lpg": "'$7'","motion": "'$8'","smoke": "'$9'","temp": "'$10'"},"schema": {"fields": [{"field": "ts","optional": false,"type": "string"},{"field": "sensor","optional": false,"type": "string"},{"field": "co","optional": false,"type": "string"},{"field": "humidity","optional": false,"type": "string"},{"field": "light","optional": false,"type": "string"},{"field": "lpg","optional": false,"type": "string"},{"field": "motion","optional": false,"type": "string"},{"field": "smoke","optional": false,"type": "string"},{"field": "temp","optional": false,"type": "string"}],"name": "iot","optional": false,"type": "struct"}}'
+    echo '{"payload": {"ts": "'$1 $2'","co": "'$4'","humidity": "'$5'","light": "'$6'","lpg": "'$7'","motion": "'$8'","smoke": "'$9'","temp": "'{$10}'"},"schema": {"fields": [{"field": "ts","optional": false,"type": "string"},{"field": "co","optional": false,"type": "string"},{"field": "humidity","optional": false,"type": "string"},{"field": "light","optional": false,"type": "string"},{"field": "lpg","optional": false,"type": "string"},{"field": "motion","optional": false,"type": "string"},{"field": "smoke","optional": false,"type": "string"},{"field": "temp","optional": false,"type": "string"}],"name": "iot","optional": false,"type": "struct"}}'
 }
 
 TOPICS=()
 
 for file in `find $1 -name \*simulate_sensor.txt` ; do
     echo $file
-    LOCATION="topic"
     head -10 $file |while read -r line ; do
         SENSOR=`echo ${line} | awk '{ print $3 }'`
         if [[ ! " ${TOPICS[@]} " =~ " ${SENSOR} " ]]; then
