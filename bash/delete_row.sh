59948a4640498d9ab0ecf40b8e061072a6219b4a
Israel Imru <imru@fixstars.com>
2025-01-08 12:47:07 -0800

    added content

--- bash/delete_row.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -i --location --request DELETE "${GRIDDB_WEBAPI_URL}/containers/deviceMaster/rows" \
---header 'Content-Type: application/json' \
---header "Authorization: Basic ${USER_PASS}"  \
---data '[
-  "device1"
-]'
